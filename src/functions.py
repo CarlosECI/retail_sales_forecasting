fb3827b771205c12377591896b32558cb91bb5cd
CarlosECI <carlosedcaib@gmail.com>
2026-06-22 00:26:31 +0200

    lightgbm testing

--- src/functions.py
+++ src/functions.py
@@ -1,21 +1,38 @@
 from sklearn.metrics import mean_absolute_percentage_error
 import numpy as np
+import pandas as pd
 
-def walk_forward_validation(serie, modelo_func, min_train=96, horizonte=3):
+def walk_forward_validation(data, modelo_func, min_train=96, horizonte=3, features=None, target=None):
     errores = []
-    n = len(serie)
+    n = len(data)
     
-    for i in range(min_train, n - horizonte + 1, horizonte):
-        train = serie.iloc[:i]
-        test = serie.iloc[i: i + horizonte]
-        
-        try:
-            predicciones = modelo_func(train, horizonte)
-            mape = mean_absolute_percentage_error(test, predicciones)
-            errores.append(mape)
-        except Exception as e:
-            print(f'Error en ventana {i}: {e}')
-            continue
+    if features is not None and target is not None:
+        for i in range(min_train, n - horizonte + 1, horizonte):
+            X = data[features].iloc[:i,:]
+            y = data[target].iloc[:i]
+            y_test = data[target].iloc[i:i + horizonte]
+            
+            X_test = data[features].iloc[i: i + horizonte]
+            
+            try:
+                predicciones = modelo_func(X, y, X_test)
+                mape = mean_absolute_percentage_error(y_test, predicciones)
+                errores.append(mape)
+            except Exception as e:
+                print(f'Error en ventana {i}: {e}')
+                continue
+    else:
+        for i in range(min_train, n - horizonte + 1, horizonte):
+            train = data.iloc[:i]
+            test = data.iloc[i: i + horizonte]
+            
+            try:
+                predicciones = modelo_func(train, horizonte)
+                mape = mean_absolute_percentage_error(test, predicciones)
+                errores.append(mape)
+            except Exception as e:
+                print(f'Error en ventana {i}: {e}')
+                continue
         
     return {
         'mape_medio': np.mean(errores),
